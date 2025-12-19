c3cb83a92619d842969aa7acabe7bb59c2d40173
VisheshPanwar2003 <142052984+VisheshPanwar2003@users.noreply.github.com>
2025-12-20 00:31:56 +0530

    Update appointments.js

--- api/appointments.js
+++ api/appointments.js
@@ -1,5 +1,28 @@
-export default function handler(req, res) {
-  res.json({
-    envExists: !!process.env.MONGODB_URI
-  });
+import { MongoClient } from "mongodb";
+
+const uri = process.env.MONGODB_URI1;
+let client;
+
+export default async function handler(req, res) {
+  try {
+    if (!uri) {
+      throw new Error("MongoDB URI missing");
+    }
+
+    if (!client) {
+      client = new MongoClient(uri);
+      await client.connect();
+    }
+
+    const db = client.db("websiteDB");
+    const data = await db
+      .collection("appointments")
+      .find({})
+      .toArray();
+
+    res.status(200).json(data);
+  } catch (err) {
+    console.error("Mongo Error:", err);
+    res.status(500).json({ error: err.message });
+  }
 }
