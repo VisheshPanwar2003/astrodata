4fc4d76626dbf2e0782fdd0d7122f2185bb56ae9
VisheshPanwar2003 <142052984+VisheshPanwar2003@users.noreply.github.com>
2025-12-20 00:50:00 +0530

    Update appointments.js

--- api/appointments.js
+++ api/appointments.js
@@ -1,25 +1,6 @@
-import { MongoClient } from "mongodb";
-
-const uri = process.env.MONGODB_URI;
-let client;
-
-export default async function handler(req, res) {
-  try {
-    if (!uri) {
-      throw new Error("MongoDB URI missing");
-    }
-
-    if (!client) {
-      client = new MongoClient(uri);
-      await client.connect();
-    }
-
-    const db = client.db("websiteDB");
-    const data = await db.collection("appointments").find({}).toArray();
-
-    res.status(200).json(data);
-  } catch (err) {
-    console.error(err);
-    res.status(500).json({ error: err.message });
-  }
+export default function handler(req, res) {
+  res.status(200).json({
+    status: "API is working",
+    envKeys: Object.keys(process.env)
+  });
 }
