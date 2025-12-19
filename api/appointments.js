45de7b42e0cf01cf99fd4e1e4ae3c442b3e1d4bf
VisheshPanwar2003 <142052984+VisheshPanwar2003@users.noreply.github.com>
2025-12-20 00:20:36 +0530

    Update appointments.js

--- api/appointments.js
+++ api/appointments.js
@@ -1,8 +1,5 @@
 export default function handler(req, res) {
-  res.status(200).json({
-    envExists: !!process.env.MONGODB_URI,
-    envValueStart: process.env.MONGODB_URI
-      ? process.env.MONGODB_URI.substring(0, 15)
-      : null
+  res.json({
+    envExists: !!process.env.MONGODB_URI
   });
 }
